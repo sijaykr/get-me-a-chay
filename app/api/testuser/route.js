63fe39ac5c1024b597b2f9ee7f4995264993210f
SANJAYKUMAR <123081257+sijaykr@users.noreply.github.com>
2025-09-07 01:53:26 +0530

    Update route.js

--- app/api/testuser/route.js
+++ app/api/testuser/route.js
@@ -1,1 +1,12 @@
 
+// /app/api/testuser/route.js
+import { fetchuser } from "@/actions/userserveraction";
+
+export async function GET() {
+  try {
+    const user = await fetchuser("testusername"); // replace with a real username
+    return new Response(JSON.stringify(user), { status: 200 });
+  } catch (err) {
+    return new Response(err.message, { status: 500 });
+  }
+}
